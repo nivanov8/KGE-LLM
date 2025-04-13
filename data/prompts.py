83f9a7c998e337245184962f9b411846f6787951
Samanvay Vajpayee <samanvayvajpayee@Samanvays-MacBook-Pro.local>
2025-04-13 12:17:41 -0400

    token counting for prompting

---
+++ data/prompts.py
@@ -0,0 +1,39 @@
+import tiktoken
+from fb15k_dataloaders import FB15k237DataModule
+
+enc = tiktoken.encoding_for_model("gpt-4o")
+test_token_count = 0
+
+
+def format_prompt(head, relation):
+    return [
+        {
+            "role": "system",
+            "content": (
+                "You are an expert knowledge graph completor. You will be given a head and a relation. "
+                "Your task is to use that information and find a tail entity which is a result of the relation "
+                "being applied to the head entity. Give your answer in as few words as possible."
+            )
+        },
+        {
+            "role": "user",
+            "content": f"HEAD ENTITY: {head}\n\nRELATION: {relation}\n\nTAIL ENTITY:"
+        }
+    ]
+
+def format_string_prompt(prompt):
+    return '\n'.join([f"{p['role'].upper()}: {p['content']}" for p in prompt])
+
+
+
+fb_loader = FB15k237DataModule(batch_size=1)  # batch size 1 for token-level granularity
+_, _, test_loader = fb_loader.get_dataloaders()
+
+for batch in test_loader:
+    heads, relations, _ = batch
+    for h, r in zip(heads, relations):
+        prompt = format_prompt(h, r)
+        string_prompt = format_string_prompt(prompt)
+        test_token_count += len(enc.encode(string_prompt))  # len of encoded token list
+
+print(f"Total tokens used in test prompts: {test_token_count}")
